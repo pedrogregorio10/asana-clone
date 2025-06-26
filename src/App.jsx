b0970a8148f3dd7ce092a1c1f1355a9d6a1bb822
Pedro Gregorio <pedrogregoriopg9@gmail.com>
2025-06-26 13:36:35 +0100

    layout set

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { DynamicIcon } from 'lucide-react/dynamic';
 
+import Header from './componets/Header';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen flex flex-col gap-3.5">
+         <div className="bg-neutral-700 text-neutral-300">
+            <h1>Minhas tarefas</h1>
+           
+         </div>
+           
+
+         <main className="flex-1 overflow-y-auto bg-neutral-800 text-neutral-300 px-4 py-6 justify-center flex">
+              <p>As tarefas aparecem aqui</p>
+         </main>
+
+         {/*Addion Tasks*/}
+          <div className='inline-flex justify-center'>
+             <div className='bg-neutral-700 p-2'>
+               <button>
+                  <DynamicIcon name='settings-2'/>
+               </button>
+               <button>
+                  <DynamicIcon name='chevrons-up-down'/>
+               </button>
+               <button>
+                 <DynamicIcon name='plus'/>
+               </button>
+             </div>
+          </div>
+
+        {/*HEADER SECTION */}
+        <Header />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
