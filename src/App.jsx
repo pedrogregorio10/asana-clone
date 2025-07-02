cf05ebce52f7710b7e2bdcee5999a61042c58c5b
Pedro Gregorio <pedrogregoriopg9@gmail.com>
2025-07-02 23:45:55 +0100

    df

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="h-screen flex flex-col gap-3.5">
     
 
-        <h1 className="text-3xl"> Leticia da a merda da minha parte do dinheiro</h1>
+        <h1 className="text-3xl text-red-600"> Leticia da a merda da minha parte do dinheiro</h1>
 
         {/*Addion Tasks*/}
    
